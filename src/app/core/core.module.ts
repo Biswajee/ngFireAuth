7212671420bcb7333888839de69e8b32e3889028
Biswajee <roy.biswajeet161@gmail.com>
2020-08-02 02:18:49 +0530

    Setup firestore n fireauth

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ]
 })
 export class CoreModule { }
